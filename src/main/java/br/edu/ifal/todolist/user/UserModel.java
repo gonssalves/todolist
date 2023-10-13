8eed9e77e6d517ab81a6e49a5c71df88f48a7cb9
gonssalves <vgo1@ifal.edu.br>
2023-10-13 11:39:31 -0300

    feat: add unique constraint and created date column

--- src/main/java/br/edu/ifal/todolist/user/UserModel.java
+++ src/main/java/br/edu/ifal/todolist/user/UserModel.java
@@ -1,7 +1,11 @@
 package br.edu.ifal.todolist.user;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,8 +20,12 @@ public class UserModel {
     private UUID id;
     
     // @Column(name = "usuario")
+    @Column(unique=true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
