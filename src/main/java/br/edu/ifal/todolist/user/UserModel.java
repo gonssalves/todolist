71fe6e5a83fdb53cf6c7028ac1a01bdaf6e6827a
gonssalves <vgo1@ifal.edu.br>
2023-10-13 11:38:27 -0300

    feat: add auto-generated id

--- src/main/java/br/edu/ifal/todolist/user/UserModel.java
+++ src/main/java/br/edu/ifal/todolist/user/UserModel.java
@@ -1,9 +1,21 @@
 package br.edu.ifal.todolist.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name ="tbl_users")
 public class UserModel {
+    
+    @Id
+    @GeneratedValue(generator= "UUID")
+    private UUID id;
+    
+    // @Column(name = "usuario")
     private String username;
     private String name;
     private String password;
