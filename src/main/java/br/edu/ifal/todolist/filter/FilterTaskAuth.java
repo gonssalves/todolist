391e871e5c25c45bc72262f114599478eb45d006
gonssalves <vgo1@ifal.edu.br>
2023-10-13 17:00:34 -0300

    feat: send error message if username is not found

--- src/main/java/br/edu/ifal/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/edu/ifal/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,11 @@ package br.edu.ifal.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.edu.ifal.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -38,13 +43,18 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 
 
         // Validar usuário
-
-        // Validar senha
-
-        // Seguir viagem
-        
-        filterChain.doFilter(request, response);     
-    
+        var user = this.userRepository.findByUsername(username);
+        if(user == null) {
+            response.sendError(401);
+        } else {
+            
+            // Validar senha
+            
+            // Seguir viagem
+            
+            filterChain.doFilter(request, response);     
+        }
+            
           
     }
 
